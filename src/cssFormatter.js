949edcf66a6999a877d7182bc5644d9af1a041fc
chocolateimage <chocolateimage@protonmail.com>
2024-12-18 19:22:00 +0100

    Separate rule order into a separate file

--- src/cssFormatter.js
+++ src/cssFormatter.js
@@ -1,84 +1,12 @@
 const vscode = require("vscode");
 const allCSSProperties = require("./allCSSProperties");
+const { rawRuleOrder } = require("./ruleOrder");
 
 function getRuleOrder() {
     // Modified from https://9elements.com/css-rule-order/
-    const original = [
-        // GENERATED CONTENT
-        "content",
-        null,
-        // POSITION AND LAYOUT
-        "z-index",
-        "position",
-        "top",
-        "bottom",
-        "left",
-        "right",
-        "float",
-        "clear",
-        null,
-        // DISPLAY AND VISIBILITY
-        "display",
-        "flex",
-        "justify-content",
-        "align-items",
-        "align-self",
-        null,
-        "opacity",
-        "transform",
-        null,
-        // CLIPPING
-        "overflow",
-        "clip",
-        null,
-        // ANIMATION
-        "animation",
-        "transition",
-        null,
-        // BOX MODEL (FROM OUTSIDE IN)
-        "margin",
-        "margin-top",
-        "margin-right",
-        "margin-bottom",
-        "margin-left",
-        "margin-inline",
-        "margin-block",
-        null,
-        "box-shadow",
-        "border",
-        "border-radius",
-        "box-sizing",
-        null,
-        "min-width",
-        "min-height",
-        "width",
-        "height",
-        "max-width",
-        "max-height",
-        null,
-        "padding",
-        "padding-top",
-        "padding-right",
-        "padding-bottom",
-        "padding-left",
-        "padding-inline",
-        "padding-block",
-        null,
-        // BACKGROUND
-        "background",
-        "cursor",
-        null,
-        // TYPOGRAPHY
-        "font",
-        "text",
-        "line-height",
-        "word-spacing",
-        "color",
-    ];
-
     const ruleOrder = [];
 
-    for (const i of original) {
+    for (const i of rawRuleOrder) {
         if (i == null) {
             ruleOrder.push(null);
             continue;
